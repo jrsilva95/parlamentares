f9bc61d5fd7e8f2db206167b7660667bda5a9a83
Ivanildo Junior <ivanildo@outlook.com>
2024-11-08 16:47:16 -0300

    listando parlamentares

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const baseUrl = "https://dadosabertos.camara.leg.br/api/v2/"
 
 const partidosSelect = document.getElementById("partidoSelect");
+const parlamentaresList = document.getElementById("parlamentares");
 
 function init(){
     getPartidos();
@@ -23,7 +24,7 @@ async function getPartidos(url){
         const dados = json.dados;
 
         for(var partido of dados){
-            partidosSelect.innerHTML += "<option value=\""+ partido.id +"\">" + partido.nome + "</option>";
+            partidosSelect.innerHTML += "<option value=\""+ partido.sigla +"\">" + partido.nome + "</option>";
         }
 
         const links = json.links;
@@ -42,4 +43,50 @@ async function getPartidos(url){
 
 }
 
+function changeListParlamentares(){
+
+    parlamentaresList.innerHTML = '';
+
+    getParlamentares(partidosSelect.value);
+
+}
+
+async function getParlamentares(sigla, url){
+
+    if(!url){
+        url = baseUrl + "deputados?siglaPartido=" + sigla;
+    }
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`Response status: ${response.status}`);
+        }
+    
+        const json = await response.json();
+        
+        const dados = json.dados;
+
+        for(var partido of dados){
+            partidosSelect.innerHTML += "<li>" + partido.nome + "</li>";
+        }
+
+        const links = json.links;
+
+        for(var link of links){
+            if(link.rel == "next"){
+                getParlamentares(sigla, link.href)
+            }
+        }
+
+        console.log("Carregou parlamentares", url);
+
+      } catch (error) {
+        console.error(error.message);
+      }
+
+}
+
 document.addEventListener('DOMContentLoaded', init, false);
+
+partidosSelect.addEventListener('change', changeListParlamentares, false);
