ba6aada9f616adb13adf9fe56bb0e29e6d877719
Ivanildo Junior <ivanildo@outlook.com>
2024-11-08 16:27:58 -0300

    fix select partidos

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const partidosSelect = document.getElementById("partidoSelect");
 
 async function getPartidos(){
 
-    const url = baseUrl + "partidos";
+    let url = baseUrl + "partidos";
 
     try {
         const response = await fetch(url);
@@ -16,11 +16,19 @@ async function getPartidos(){
         
         const dados = json.dados;
 
-        for(partido in dados){
+        for(var partido in dados){
             partidosSelect.innerHTML += "<option value=\""+ partido.id +"\">" + partido.nome + "</option>";
         }
 
-        console.log("Carregou partidos");
+        const links = json.links;
+
+        for(var link in links){
+            if(link.rel.contains("next")){
+                url = link.href;
+            }
+        }
+
+        console.log("Carregou partidos", url);
 
       } catch (error) {
         console.error(error.message);
